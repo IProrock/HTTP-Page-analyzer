b8a131e3d111f719d6ab578a2091bfafd6640da5
Ilya <iprorock@gmail.com>
2023-07-04 21:27:20 +0300

    Step 2

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/c14416be89eb355ebd0a/maintainability)](https://codeclimate.com/github/IProrock/java-project-72/maintainability)
 
 [![Test Coverage](https://api.codeclimate.com/v1/badges/c14416be89eb355ebd0a/test_coverage)](https://codeclimate.com/github/IProrock/java-project-72/test_coverage)
+
+https://project-four.onrender.com/
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,15 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.JavalinRenderer;
+import io.javalin.rendering.markdown.JavalinCommonmark;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.*;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import org.thymeleaf.TemplateEngine;
 
 public class App {
 
@@ -29,6 +38,8 @@ public class App {
             if (!isProduction()) {
                 config.plugins.enableDevLogging();
             }
+            JavalinThymeleaf.init(getTemplateEngine());
+
         });
 
         addRoutes(app);
@@ -38,7 +49,25 @@ public class App {
 
     private static void addRoutes(Javalin app) {
         app.get("/", ctx -> {
-            ctx.result("Hello World");
+//            ctx.result("Hello World");
+            ctx.render("./src/main/resources/templates/index.html");
         });
+    }
+
+
+    private static TemplateEngine getTemplateEngine() {
+        // Создаём инстанс движка шаблонизатора
+        TemplateEngine templateEngine = new TemplateEngine();
+        // Добавляем к нему диалекты
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+        // Настраиваем преобразователь шаблонов, так, чтобы обрабатывались
+        // шаблоны в директории /templates/
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        // Добавляем преобразователь шаблонов к движку шаблонизатора
+        templateEngine.addTemplateResolver(templateResolver);
+
+        return templateEngine;
     }
 }
